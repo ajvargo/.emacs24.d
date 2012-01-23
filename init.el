00091566e17fcd405db3865969590bdfc1d36b6a
Andrew J Vargo <ajvargo@computer.org>
2012-01-22 21:32:42 -0500

    Set customizations to custom location; add .gitignore

--- init.el
+++ init.el
@@ -1,1 +1,10 @@
 ;; Emacs 24
+(require 'cl) ; fancy common lisp extensions
+
+;; set directories I'll be needing
+(setq ajv-dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+
+;; puts customization in their own file
+(setq custom-file (concat ajv-dotfiles-dir "/custom.el"))
+(load custom-file 'noerror) ;; don't choke if its empty
+
