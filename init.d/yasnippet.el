7a3766198cb2f2d4da77bac22a385e70616a3695
Andrew J Vargo <ajvargo@computer.org>
2012-02-12 15:51:28 -0500

    Smarten up my snippets a bit

--- init.d/yasnippet.el
+++ init.d/yasnippet.el
@@ -1,3 +1,3 @@
 (require 'yasnippet)
-(setq yas/snippet-dirs (list (expand-file-name "snippets" ajv-emacs-config-dir) (expand-file-name "yasnippet/snippets" ajv-packages-dir)))
+(setq yas/snippet-dirs (list (expand-file-name "snippets/" ajv-emacs-config-dir) (expand-file-name "yasnippet/snippets/" ajv-packages-dir)))
 (yas/global-mode 1)
