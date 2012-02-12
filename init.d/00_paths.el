c218527a45766b5c3e26a9b96cc925dbfea632ae
Andrew J Vargo <ajvargo@computer.org>
2012-02-12 15:51:54 -0500

    Tell emacs to tell git that emacs is the editor it wants.

--- init.d/00_paths.el
+++ init.d/00_paths.el
@@ -2,4 +2,6 @@
 
 ;; Add stuff I need to my shell load path
 (setenv "PATH" (concat "/usr/local/bin:/usr/local/git/bin/git:" (getenv "PATH")))
+(setenv "GIT_EDITOR" "/usr/local/Cellar/emacs/HEAD/bin/emacsclient")
+(setenv "ALTERNATE_EDITOR" "/usr/local/Cellar/emacs/HEAD/bin/emacsclient")
 (setq exec-path (append exec-path '("/usr/local/bin" )))
