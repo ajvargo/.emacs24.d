29352d49d56e1da82962e8e6133de65f72381ba2
Andrew J Vargo <ajvargo@computer.org>
2012-04-08 19:48:57 -0400

    Add experimental.el for things I am toying with

---
+++ init.d/experimental.el
@@ -0,0 +1,50 @@
+(setq header-line-format nil
+      mode-line-format mode-line-format)
+
+(defvar indirect-mode-name nil
+  "Mode to set for indirect buffers.")
+(make-variable-buffer-local 'indirect-mode-name)
+
+(defun indirect-region (start end)
+  "Edit the current region in another buffer.
+    If the buffer-local variable `indirect-mode-name' is not set, prompt
+    for mode name to choose for the indirect buffer interactively.
+    Otherwise, use the value of said variable as argument to a funcall."
+  (interactive "r")
+  (let ((buffer-name (generate-new-buffer-name "*indirect*"))
+        (mode
+         (if (not indirect-mode-name)
+             (setq indirect-mode-name
+                   (intern
+                    (completing-read
+                     "Mode: "
+                     (mapcar (lambda (e)
+                               (list (symbol-name e)))
+                             (apropos-internal "-mode$" 'commandp))
+                     nil t)))
+           indirect-mode-name)))
+    (pop-to-buffer (make-indirect-buffer (current-buffer) buffer-name))
+    (funcall mode)
+    (narrow-to-region start end)
+    (goto-char (point-min))
+    (shrink-window-if-larger-than-buffer)))
+
+
+(defun ido-for-mode(prompt the-mode)
+  (switch-to-buffer
+   (ido-completing-read prompt
+                        (save-excursion
+                          (delq
+                           nil
+                           (mapcar (lambda (buf)
+                                     (when (buffer-live-p buf)
+                                       (with-current-buffer buf
+                                         (and (eq major-mode the-mode)
+                                              (buffer-name buf)))))
+                                   (buffer-list)))))))
+
+(defun ido-shell-buffer()
+  (interactive)
+  (ido-for-mode "Shell:" 'shell-mode))
+
+(global-set-key (kbd "C-c s") 'ido-shell-buffer)
