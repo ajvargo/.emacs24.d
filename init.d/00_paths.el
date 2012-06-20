009c541aed6b8c09af2f84e5a97f36ffd8bfdc7e
Andrew J Vargo <avargo@dataxu.com>
2012-06-20 15:57:32 -0400

    IT shouldn't be so hard to find git

--- init.d/00_paths.el
+++ init.d/00_paths.el
@@ -1,7 +1,7 @@
 (setq epg-gpg-program "/usr/local/bin/gpg")
 
 ;; Add stuff I need to my shell load path
-(setenv "PATH" (concat "/usr/local/bin:/usr/local/git/bin/git:" (getenv "PATH")))
+(setenv "PATH" (concat "/usr/local/bin:/usr/local/git/bin/git:/usr/local/git:/usr/local/git/bin:" (getenv "PATH")))
 (setenv "GIT_EDITOR" "/usr/local/Cellar/emacs/HEAD/bin/emacsclient")
 (setenv "ALTERNATE_EDITOR" "/usr/local/Cellar/emacs/HEAD/bin/emacsclient")
 (setq exec-path (append exec-path '("/usr/local/bin" )))
